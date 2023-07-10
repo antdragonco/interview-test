6075a6077bf45f104c3c2659c2cc07ad766be1fc
Kani Selvan <54349075+kaniselvan96@users.noreply.github.com>
2023-07-11 02:06:18 +0800

    developed modal to add task data

--- NavigationApp/src/screens/Home.tsx
+++ NavigationApp/src/screens/Home.tsx
@@ -1,18 +1,57 @@
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, View, FlatList, StyleSheet, Text} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Modal,
+  TextInput,
+} from 'react-native';
 
 const Home = () => {
   type dataType = {id: string; title: string; description: string};
 
   const Item = ({id, title, description}: dataType) => (
     <View key={id} style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingBottom: 10,
+        }}>
+        <Text style={styles.title}>{title}</Text>
+        <TouchableOpacity onPress={() => handelDelete(item.id)}>
+          <Text style={styles.txt_del}>Delete</Text>
+        </TouchableOpacity>
+        {/* <TouchableOpacity onPress={() => handleEdit(item)}>
+          <Text style={styles.txt_edit}>Edit</Text>
+        </TouchableOpacity> */}
+      </View>
       <Text style={styles.desc}>{description}</Text>
     </View>
   );
 
   const [data, setData] = useState<dataType[]>([]);
   const [loading, setLoading] = useState(true);
+  const [visible, setViisble] = useState(false);
+  const [hideId, setHideId] = useState(null);
+  const [title, setTitle] = useState();
+  const [desc, setDesc] = useState();
+
+  const handleVisibleModal = () => {
+    setViisble(!visible);
+    setHideId(null);
+  };
+
+  const onChangeTitle = value => {
+    setTitle(value);
+  };
+
+  const onChangeDesc = value => {
+    setDesc(value);
+  };
 
   const url = 'http://localhost:3000/items';
   useEffect(() => {
@@ -26,14 +65,45 @@ const Home = () => {
   return (
     <SafeAreaView>
       <View style={styles.container}>
+        <View style={styles.header_container}>
+          <Text style={styles.txt_main}>List of Tasks</Text>
+          <TouchableOpacity
+            onPress={handleVisibleModal}
+            style={styles.btnNewContainer}>
+            <Text style={styles.textButton}>New Course</Text>
+          </TouchableOpacity>
+        </View>
+        <Modal animationType="slide" visible={visible}>
+          <SafeAreaView>
+            <View style={styles.form}>
+              <TouchableOpacity onPress={handleVisibleModal}>
+                <Text style={styles.txtClose}>Close</Text>
+              </TouchableOpacity>
+              <TextInput
+                value={title}
+                style={styles.text_input}
+                placeholder="Title"
+                onChangeText={onChangeTitle}
+              />
+              <TextInput
+                value={desc}
+                style={styles.text_input}
+                placeholder="Desc"
+                onChangeText={onChangeDesc}
+              />
+            </View>
+          </SafeAreaView>
+        </Modal>
         <FlatList
           data={data}
           renderItem={({item}) => (
-            <Item
-              id={item.id}
-              title={item.title}
-              description={item.description}
-            />
+            <View>
+              <Item
+                id={item.id}
+                title={item.title}
+                description={item.description}
+              />
+            </View>
           )}
         />
       </View>
@@ -46,7 +116,6 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     padding: 5,
-    paddingTop: 30,
     height: '100%',
     backgroundColor: 'white',
   },
@@ -67,5 +136,59 @@ const styles = StyleSheet.create({
   },
   desc: {
     fontSize: 14,
+  },
+  txt_del: {
+    fontSize: 14,
+    marginTop: 5,
+    color: 'red',
+    fontWeight: 'bold',
+  },
+  txt_edit: {
+    fontSize: 14,
+    marginTop: 5,
+    color: 'blue',
+    fontWeight: 'bold',
+  },
+  header_container: {
+    padding: 20,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  txt_main: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  btnNewContainer: {
+    padding: 10,
+    backgroundColor: 'green',
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: {width: 3, height: 3},
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+  },
+  textButton: {
+    textAlign: 'center',
+    color: '#FFF',
+  },
+  txtClose: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'red',
+    marginVertical: 10,
+    textAlign: 'right',
+  },
+  form: {
+    padding: 20,
+    // backgroundColor : "#e3e3e3",
+    marginTop: 10,
+  },
+  text_input: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 10,
+    marginTop: 10,
   },
 });
